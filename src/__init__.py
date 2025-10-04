8fb545f56328dd8e561f74744529ad834bb2463c
MohammadmahdiRahimi <mm2000r2000@gmail.com>
2025-10-04 19:21:35 +0200

    Clean up repository: remove redundant documentation files

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,5 @@
-"""Root package for GPT-2 Human Variability Fine-tuning."""
+"""Root package for Human Variability in Language Models."""
 
 __version__ = "1.0.0"
 __author__ = "Mohammadmahdi Rahimi, Ori Brand"
-__description__ = "Fine-tuning GPT-2 with multiple labels to reproduce human linguistic variability"
+__description__ = "Fine-tuning language models (GPT-2, Mistral) with multiple labels to reproduce human linguistic variability"
